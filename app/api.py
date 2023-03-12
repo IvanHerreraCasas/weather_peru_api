4d763e940d2efa90f0e38bb13394ef6d458892b6
Ivan-H-C <ivanhc.pp@gmail.com>
2023-03-12 16:31:16 -0500

    feat(statistics_resource): create statisti records resource

--- app/api.py
+++ app/api.py
@@ -2,10 +2,12 @@ from flask_restful import Api
 
 from app.resources.station_resource import StationsResource
 from app.resources.record_resource import RecordsResource
+from app.resources.statistics_resource import StatisticRecordsResource
 
 api = Api()
 
 api.add_resource(StationsResource, '/stations')
 api.add_resource(RecordsResource, '/records/<station_id>')
+api.add_resource(StatisticRecordsResource, '/statistic/records')
 
 
---
+++ app/resources/statistics_resource.py
@@ -0,0 +1,32 @@
+from flask import request, jsonify
+from flask_restful import Resource
+from datetime import datetime
+
+from app.service.record_service import get_statistical_record
+from app.service.station_service import get_station_by_name
+
+
+class StatisticRecordsResource(Resource):
+
+    def get(self):
+        params = request.args
+
+        start_date = params.get('start_date')
+        end_date = params.get('end_date')
+
+        record, error = get_statistical_record(
+            start_date=datetime.strptime(start_date, '%Y-%m-%d') if start_date is not None else None,
+            end_date=datetime.strptime(end_date, '%Y-%m-%d') if end_date is not None else None,
+            stat_type=params.get('stat_type'), parameter=params.get('parameter'),
+            region=params.get('region'), province=params.get('province'),
+            district=params.get('district'),
+            station_name=params.get('station_name'))
+
+        if record is not None and error == '':
+            station = get_station_by_name(record.station_name)
+            return jsonify({
+                    'record': record.to_dict(),
+                    'station': station.to_dict()
+                })
+
+        return {'message': error}, 400
