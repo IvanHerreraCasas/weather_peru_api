01b66774d28b229567d37d63b906b5a345f5043e
Ivan-H-C <ivanhc.pp@gmail.com>
2023-03-12 10:18:54 -0500

    feat(service): create station service

---
+++ app/service/station_service.py
@@ -0,0 +1,53 @@
+from sqlalchemy import and_
+
+from app.database import db
+from app.models import Station
+
+
+def save_station(data):
+    name = data.get('name')
+    region = data.get('region')
+    province = data.get('province')
+    district = data.get('district')
+
+    station = db.session.query(Station).filter_by(name=name).first()
+
+    if station:
+        station.region = region
+        station.province = province
+        station.district = district
+        db.session.commit()
+        return station
+
+    station = Station(name=name, region=region, province=province, district=district)
+    db.session.add(station)
+    db.session.commit()
+
+    return station
+
+
+def get_station(station_id):
+    station = db.get_or_404(Station, station_id)
+
+    return station
+
+
+def get_available_stations(region, province, district):
+
+    filters = []
+
+    if region is not None:
+        filters.append(Station.region == region)
+
+    if province is not None:
+        filters.append(Station.province == province)
+
+    if district is not None:
+        filters.append(Station.district == district)
+
+    stations = db.session.query(Station).where(and_(*filters)).all()
+
+    return stations
+
+
+
