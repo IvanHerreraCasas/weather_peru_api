6f2e8f5be4bfc2455fd0560e3f7b7c703a5b35fd
Ivan-H-C <ivanhc.pp@gmail.com>
2023-03-12 16:29:10 -0500

    feat(record): add to_dict function

--- app/models/record.py
+++ app/models/record.py
@@ -10,3 +10,13 @@ class Record(db.Model):
     min_temp = db.Column(db.Float, nullable=True)
     max_temp = db.Column(db.Float, nullable=True)
     station_name = db.Column(db.String(30), db.ForeignKey('stations.name'), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'date': self.date.strftime('%Y-%m-%d'),
+            'precipitation': self.precipitation,
+            'max_temp': self.max_temp,
+            'min_temp': self.min_temp,
+            'station_name': self.station_name
+        }
