c3d4eb21867eee934cd8ed095cdce452bc981c94
Ivan-H-C <ivanhc.pp@gmail.com>
2023-03-12 18:26:53 -0500

    feat(statistics_resource): create statistic value resource

--- app/api.py
+++ app/api.py
@@ -2,12 +2,13 @@ from flask_restful import Api
 
 from app.resources.station_resource import StationsResource
 from app.resources.record_resource import RecordsResource
-from app.resources.statistics_resource import StatisticRecordsResource
+from app.resources.statistics_resource import StatisticRecordsResource, StatisticValueResource
 
 api = Api()
 
 api.add_resource(StationsResource, '/stations')
 api.add_resource(RecordsResource, '/records/<station_id>')
 api.add_resource(StatisticRecordsResource, '/statistic/records')
+api.add_resource(StatisticValueResource, '/statistic/value')
 
 
--- app/resources/statistics_resource.py
+++ app/resources/statistics_resource.py
@@ -2,7 +2,7 @@ from flask import request, jsonify
 from flask_restful import Resource
 from datetime import datetime
 
-from app.service.record_service import get_statistical_record
+from app.service.record_service import get_statistical_record, get_statistical_value
 from app.service.station_service import get_station_by_name
 
 
@@ -28,5 +28,27 @@ class StatisticRecordsResource(Resource):
                     'record': record.to_dict(),
                     'station': station.to_dict()
                 })
+
+        return {'message': error}, 400
+
+class StatisticValueResource(Resource):
+    def get(self):
+        params = request.args
+
+        start_date = params.get('start_date')
+        end_date = params.get('end_date')
+
+        value, error = get_statistical_value(
+            start_date=datetime.strptime(start_date, '%Y-%m-%d') if start_date is not None else None,
+            end_date=datetime.strptime(end_date, '%Y-%m-%d') if end_date is not None else None,
+            stat_type=params.get('stat_type'), parameter=params.get('parameter'),
+            region=params.get('region'), province=params.get('province'),
+            district=params.get('district'),
+            station_name=params.get('station_name'))
+
+        if value is not None and error == '':
+            return jsonify({
+                'value': value
+            })
 
         return {'message': error}, 400
